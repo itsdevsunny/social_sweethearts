0e3ad4a9b58b1eecb6640468eff087382624e106
Savera <iamthesunnykumar@gmail.com>
2019-06-03 21:58:50 +0530

    getting image as json response

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 
+from django.views.decorators.csrf import csrf_exempt
+
 from social_django.models import UserSocialAuth
 
 import requests
@@ -13,11 +15,18 @@ def Profile(request):
 		except UserSocialAuth.DoesNotExist:
 			facebook_auth = None
 		if facebook_auth:
+			# reading profile picture doesn't require an access token or permission
+			# if you want to get JSON response simply put redirect=false in your query parameter
 			FACEBOOK_PROFILE = f"https://graph.facebook.com/v3.3/{facebook_auth.uid}/picture"
-			response = requests.get(FACEBOOK_PROFILE, params={'height':300, 'width':300},)
-	return render(request, 'accounts/profile.html', {'response':response})
+			response = requests.get(FACEBOOK_PROFILE, params={'height':300, 'width':300, 'redirect':'false'})
+
+			if response.status_code == 200:
+				response.encoding = 'utf-8'
+				profile_image = response.json()['data']['url']
+	return render(request, 'accounts/profile.html', {'profile_image':profile_image})
 
 
+@csrf_exempt
 def FacebookProfileDisconnect(request):
 	if request.method == 'POST':
 		print(request.POST)
